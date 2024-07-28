df6e6c09f5e723f90c7d023a2f8f28330ed09489
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-07-28 23:23:37 +0600

    Add Access modifier image

--- ts-toutorial-1/script.ts
+++ ts-toutorial-1/script.ts
@@ -2,5 +2,40 @@ function addNumbers(num1:number, num2:number) {
   console.log(num1 + num2);
 }
 
-addNumbers(20, 30);
+// addNumbers(20, 30);
 // addNumbers(30, "40");
+
+
+function userIdDataType(userId: string | number) {
+  // console.log(typeof userId);
+  // console.log(userId);
+}
+
+userIdDataType("123"); 
+userIdDataType(123);
+
+// const isEven = (num: number | string) => {
+//   if (typeof num === "number") {
+//     console.log(typeof num);
+//     return num % 2 === 0 ? "even" : "odd";
+//   } else {
+//     console.log(typeof num);
+//     return Number(num) % 2 === 0 ? "even" : "odd";
+//   }
+// };
+
+// console.log(isEven(32));
+// console.log(isEven("25"));
+
+const isEven = (num: number | string) =>{
+  if (typeof num === "number"){
+    console.log(typeof num);
+    return num % 2 === 0 ? "even" : "odd"
+  }else{
+    console.log(typeof num);{
+      return Number(num) % 2 === 0 ? "even" : "odd"
+    }
+  }
+
+}
+console.log(isEven("50"))
